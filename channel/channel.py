e9c65dcf6d762911999b241f5bd63d6c2ade6c15
沈伟 <shenwei0329@hotmail.com>
2019-07-18 11:40:11 +0800

    Functional enhancemen
    To allow mult- input queue for a node, and in consideration of root
    node.

--- channel/channel.py
+++ channel/channel.py
@@ -14,11 +14,28 @@ class Channel:
         self.queue = []
 
     def in_q(self, event):
+        """
+        To put a event into the queue.
+        :param event:
+        :return:
+        """
         self.queue.append(event)
 
     def out_q(self):
-        _event = self.queue.pop(0)
+        """
+        To out a event from queue at one time.
+        :return: one or none.
+        """
+        if len(self.queue) == 0:
+            return None
+
+        return self.queue.pop(0)
 
     def insert_q(self, event):
+        """
+        To insert a event into header of the queue.
+        :param event: a event.
+        :return:
+        """
         self.queue.insert(0, event)
 
--- node/node.py
+++ node/node.py
@@ -12,11 +12,11 @@ class Node:
     def __init__(self):
         self.id = hashlib.sha1("%s" % time.time()).hexdigest()
         self.function = None
-        self.in_channel = None
+        self.in_channel = []
         self.out_channel = []
 
     def add_in_channel(self, ch):
-        self.in_channel = ch
+        self.in_channel.append(ch)
 
     def add_out_channel(self, ch):
         self.out_channel.append(ch)
@@ -24,14 +24,33 @@ class Node:
     def add_function(self, func):
         self.function = func
 
+    def _do_it(self, _event):
+
+        _new_event = self.function(_event)
+        if _new_event is not None:
+            if len(self.out_channel) > 0:
+
+                # 把处理结果往后传递
+                for _oq in self.out_channel:
+                    _oq.in_q(_new_event)
+
     def run(self):
+        """
+        To operate the  function of node.
+        :return: Result of event.
+        """
+
         if self.function is None:
             return
-        if self.in_channel is None:
-            return
-        _event = self.in_channel.out_q()
-        _new_event = self.function(_event)
-        if (_new_event is not None) and (len(self.out_channel) > 0):
-            for _q in self.out_channel:
-                _q.in_q(_new_event)
 
+        if len(self.in_channel) == 0:
+            """root node"""
+            self._do_it(None)
+            return
+
+        for _q in self.in_channel:
+
+            # to operate one event at one time.
+            _event = _q.out_q()
+            if _event is not None:
+                self._do_it(_event)
